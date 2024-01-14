d4f68dcb59360515fd847585383e505b92fa947c
Oleh Husak <40184919+oleh19127@users.noreply.github.com>
2024-01-14 17:12:18 +0200

    add Conversation

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,11 +1,74 @@
 import 'dotenv/config';
-import { Bot, GrammyError, HttpError } from 'grammy';
-import { logger } from '../utils/logger/logger';
 
-export const bot = new Bot(process.env.BOT_TOKEN as string);
+import {
+  Bot,
+  Context,
+  GrammyError,
+  HttpError,
+  Keyboard,
+  session,
+} from 'grammy';
+
+import {
+  type Conversation,
+  type ConversationFlavor,
+  conversations,
+  createConversation,
+} from '@grammyjs/conversations';
+
+import { logger } from '../utils/logger/logger';
+import { themeService } from '../services/ThemeService';
+
+type MyContext = Context & ConversationFlavor;
+type MyConversation = Conversation<MyContext>;
+
+export const bot = new Bot<MyContext>(process.env.BOT_TOKEN as string);
+
+bot.api.setMyCommands([
+  {
+    command: 'start',
+    description: 'Start the bot',
+  },
+  {
+    command: 'add_theme',
+    description: 'Add the theme, you want to repeat',
+  },
+]);
+
+bot.use(session({ initial: () => ({}) }));
+bot.use(conversations());
+
+async function addThemeConversation(
+  conversation: MyConversation,
+  ctx: MyContext,
+) {
+  await ctx.reply('What theme do you want to add?');
+  const { message } = await conversation.wait();
+  const result = await themeService.createTheme(message?.text as string);
+  await ctx.reply(result);
+}
+
+bot.use(createConversation(addThemeConversation));
 
 bot.command('start', async (ctx) => {
-  await ctx.reply(`Hello ${ctx.from?.first_name}`);
+  const allThemes = await themeService.getAllThemes();
+  if (allThemes.length === 0) {
+    return await ctx.reply('No themes to display');
+  }
+  const startKeyboard = new Keyboard().resized();
+  for (const theme of allThemes) {
+    startKeyboard.add(theme.themeName);
+  }
+  await ctx.reply(
+    `Hello ${ctx.from?.first_name},\n What theme do you want to repeat?`,
+    {
+      reply_markup: startKeyboard.toFlowed(3),
+    },
+  );
+});
+
+bot.command('add_theme', async (ctx) => {
+  await ctx.conversation.enter('addThemeConversation');
 });
 
 bot.catch((err) => {
